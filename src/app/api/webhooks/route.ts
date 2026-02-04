0027d25ba4ac9fd2ead406783ec3cf62fbd702ef
zeinaboud <zeinaboudaimah@gmail.com>
2026-02-04 14:28:26 +0200

    fix error

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2025-12-15.clover",
 });
 
 export async function POST(req: NextRequest) {
